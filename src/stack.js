b19227b610e36781b1e78943410d1f8de8e8846c
temaxo <melnikov.artem294@gmail.com>
2025-10-15 11:04:54 +0300

    [feat:] Stack completed

--- src/stack.js
+++ src/stack.js
@@ -12,19 +12,23 @@ const { NotImplementedError } = require("../extensions/index.js");
  * stack.pop(); // undefined
  *
  */
+
 module.exports = class Stack {
-  push(/* element */) {
-    throw new NotImplementedError("Not implemented");
-    // remove line with error and write your code here
+  push(element) {
+      if (!this.items)
+          this.items = [];
+      this.items.push(element);
   }
 
   pop() {
-    throw new NotImplementedError("Not implemented");
-    // remove line with error and write your code here
+      if(!this.items || this.items.length === 0)
+          return undefined;
+      return this.items.pop()
   }
 
   peek() {
-    throw new NotImplementedError("Not implemented");
-    // remove line with error and write your code here
+      if(!this.items || this.items.length === 0)
+          return undefined;
+      return this.items[this.items.length - 1];
   }
 };
